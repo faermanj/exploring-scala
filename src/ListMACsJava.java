fe5bddf00473fe858687864149a00399c1f34b42
denkab <denka@technologist.com>
2012-03-31 11:38:21 -0700

    Update src/ListMACsJava.java

--- src/ListMACsJava.java
+++ src/ListMACsJava.java
@@ -3,23 +3,13 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class ListMACsJava {
-	public static void main(String[] args) {
-		try {
-			Enumeration<NetworkInterface> nics = 
-						NetworkInterface.getNetworkInterfaces();
-			while(nics.hasMoreElements()){
-				NetworkInterface nic = nics.nextElement();
-				byte[] mac = nic.getHardwareAddress();
-				String macStr = "";
-				if (mac != null)
-					for (int i = 0; i < mac.length; i++) {
-						macStr += String.format("%02x", mac[i]);
-						if(i < mac.length-1) macStr += ":";
-					}
-				System.out.println(macStr);
-			}
-		} catch (SocketException e) {
-			System.err.println("Ooops!");
-		}
-	}
+    public static void main(String[] args) throws SocketException {
+        for (NetworkInterface nic : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+            byte[] mac = nic.getHardwareAddress();
+            for (int i = 0; mac != null && i < mac.length; i++) {
+                System.out.format("%2x", mac[i]);
+                System.out.print(i == mac.length - 1 ? '\n' : ':');
+            }
+        }
+    }
 }
